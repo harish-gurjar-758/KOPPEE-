ef1f228f491a6621edf0d5b7123fb8555396174a
harish-gurjar-758 <gurjarharish758@gmail.com>
2025-04-29 18:08:40 +0530

    create naveBar and hero setion in home page and design the css

--- client/src/components/Pages/Home.jsx
+++ client/src/components/Pages/Home.jsx
@@ -2,8 +2,22 @@ import React from 'react'
 
 export default function Home() {
   return (
-    <div>
-      hyy i am home page
+    <div className='Home'>
+      {/* Hero */}
+      <div className="hero">
+        <div className="hero-container">
+          <h4>
+            WELCOME TO AWAKE
+          </h4>
+          <h2>BREWING DREAMS WIDE COFFEE</h2>
+          <div className='btn-group'>
+            <div className="btn btn-border">BOOK A TABLE</div>
+            <div className="btn">OPEN MENU</div>
+          </div>
+        </div>
+      </div>
+
+      {/* About */}
     </div>
   )
 }
--- client/src/components/utils/NavBar.jsx
+++ client/src/components/utils/NavBar.jsx
@@ -1,30 +1,39 @@
 import React from 'react'
-import logo from '../assets/web-logo.png';
+import logo from '../assets/web-logo-1.png';
 import { SlHandbag } from "react-icons/sl";
 import { Link } from 'react-router-dom';
 
 export default function NavBar() {
     return (
-        <div>
+        <div className='NavBar'>
             {/* Logo */}
-            <img className='logo' src={logo} alt="" />
-            {/* Links */}
-            
-            <Link>
+            <Link className="link" to='/'>
+                <img className='logo' src={logo} alt="" />
             </Link>
-            Home
-            About
-            Menu
-            Order
-            Offer
-            Shops
-            Contact
+            {/* Links */}
 
-            {/* Reservation Button */}
-            Reservation
+            <div className='links-container'>
+                <Link to='/' className='link'><h4>Home</h4></Link>
+                <Link to='/about' className='link'><h4>About</h4></Link>
+                <Link className="link"><h4>Menu</h4></Link>
+                <Link className="link"><h4>Order</h4></Link>
+                <Link className="link"><h4>Offer</h4></Link>
+                <Link className="link"><h4>Shops</h4></Link>
+                <Link className="link"><h4>Contact</h4></Link>
+            </div>
 
-            {/* Shoping card Logo */}
-            <SlHandbag />
+            <div className='more-options'>
+                {/* Reservation Button */}
+                <button className="btn orange-btn">Reservation</button>
+                {/* Shoping card Logo */}
+                <div className='shoping-btn'>
+                    <SlHandbag className='shoping-icon' />
+                    <div className='count'>
+                        <span>0</span>
+                    </div>
+                </div>
+
+            </div>
         </div>
     )
 }
