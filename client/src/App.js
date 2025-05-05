af0041169f70eb6312a5ab73cddabdc26a474811
harish-gurjar-758 <gurjarharish758@gmail.com>
2025-05-05 15:52:37 +0530

    create adminBlock.js and set the route in app.js

--- client/src/App.js
+++ client/src/App.js
@@ -3,6 +3,8 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import NavBar from './components/utils/NavBar';
 import Home from './components/Pages/Home';
 import Reservation from './components/Pages/Reservation';
+import AdminBlock from './adminBlock/AdminBlock';
+
 
 function App() {
   return (
@@ -12,6 +14,7 @@ function App() {
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path='/reservation' element={<Reservation />} />
+          <Route path='/coffee-shop/admin-block/' element={<AdminBlock />} />
           {/* <Route path="/menu" element={<Menu />} /> */}
           {/* <Route path="/about" element={<About />} /> */}
           {/* <Route path="/contact" element={<Contact />} /> */}
---
+++ client/src/adminBlock/AdminBlock.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function AdminBlock() {
+  return (
+    <div>
+    this is admin block
+    </div>
+  )
+}
