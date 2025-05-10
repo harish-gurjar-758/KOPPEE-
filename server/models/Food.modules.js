f39d985c0d974ab5cdae53a53c70c436809cb9bd
harish-gurjar-758 <gurjarharish758@gmail.com>
2025-05-10 18:44:50 +0530

    create a food module

--- server/models/Food.modules.js
+++ server/models/Food.modules.js
@@ -1,2 +1,37 @@
 import mongoose from "mongoose";
 
+const foodSchema = new mongoose.Schema({
+    foodName: {
+        type: String,
+        required: true,
+    },
+    foodType: {
+        type: String,
+        required: true,
+    },
+    foodPrice: {
+        type: Number,
+        required: true,
+    },
+    foodImageUrl: {
+        type: String,
+        required: true,
+    },
+    foodDescount: {
+        type: Number,
+    },
+    foodDescription: {
+        type: String,
+    },
+
+    foodRatings: [
+        {
+            type: Number,
+            min: 1,
+            max: 5
+        }
+    ]
+});
+
+const Food = mongoose.model('Food', foodSchema);
+export default Food;
